650181a64ba48f996849a755f1413b68ff857dde
hunsri <richard.hunstock@gmx.de>
2021-06-24 05:14:59 +0200

    added simple walls

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -6,11 +6,15 @@ public class NPCSpawner : MonoBehaviour
 {
     [SerializeField]
     private Transform _npc;
+    [SerializeField]
+    private int _amount;
 
     // Start is called before the first frame update
     void Start()
     {
-    Instantiate(_npc, this.transform.position, Quaternion.Euler(0,0,0));
+        for(int i = 0; i <_amount; i++) {
+            Instantiate(_npc, this.transform.position, Quaternion.Euler(0,0,0));
+        }
     }
 
     // Update is called once per frame
