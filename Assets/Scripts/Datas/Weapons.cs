8d3ab814093a3a27ade3e26c86c188b785f9e243
Louis Andrew <louisandrew3@gmail.com>
2021-06-30 17:00:18 +0200

    feat: add mask throwing animation

--- Assets/Scripts/Datas/Weapons.cs
+++ Assets/Scripts/Datas/Weapons.cs
@@ -9,7 +9,7 @@ public class Weapons
     // ! double check the syringe prefab at
     // ! `/Resources/Weapons/Weapon0` and `/Prefabs/Weapons/Syringe` upon changing.
 
-    public static WeaponData Weapon1() { return new WeaponData(1, 100, new float[] { 0f, 0f, 0f }); }
+    public static WeaponData Weapon1() { return new WeaponData(1, 100, new float[] { -0.111f, 0.012f, 0.01f }); }
 
     public static WeaponData Weapon2() { return new WeaponData(2, 50, new float[] { 0f, 0f, 0f }); }
 }
